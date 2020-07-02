34fd780bac8ec54823dccbd820967176aee0fe2e
vivishen-x <651115309@qq.com>
2020-07-02 15:21:08 +0900

    add show block

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,30 @@
 import Layout from "../components/Layout";
-import TopFixBar from "../components/TopFixBar";
-import NowPlayingBar from "../components/NowPlayingBar";
 
 export default function Home() {
   return (
     <Layout meta={{}}>
       <div className="container">
-        <div style={{ height: "300px" }}>Test Content</div>
-        <div style={{ height: "300px" }}>Test Content</div>
-        <div style={{ height: "300px" }}>Test Content</div>
+        <h1 className="h1">热门搜索</h1>
+        <div className="top-recommend">Test Content</div>
+        <h1 className="h1">推荐歌单</h1>
+        <div className="top-recommend">Test Content</div>
+        <h1 className="h1">最新音乐</h1>
+        <div className="top-recommend">Test Content</div>
       </div>
       <style jsx>{`
         .container {
-          height: 100%;
+          height: auto;
+          padding-bottom: 100px;
+        }
+        .h1 {
+          font-size: 18px;
+          padding-left: 8px;
+          line-height: 1;
+          border-left: solid 3px #d43c33;
+          margin: 10px 0;
+        }
+        .top-recommend {
+          height: 300px;
         }
       `}</style>
     </Layout>
