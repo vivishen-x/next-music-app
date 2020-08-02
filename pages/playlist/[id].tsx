759e8278785e5e92dbef362e485c656b5caf9e97
vivishen-x <651115309@qq.com>
2020-08-02 17:14:12 +0900

    fix playlist UI

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -21,21 +21,46 @@ export default function Playlist({
   return (
     <Layout meta={{}}>
       <div className="playlist-header">
-        <h2>{playlist.name}</h2>
+        <div className="img-container">
+          <img src={playlist.coverImgUrl} alt={playlist.name} />
+        </div>
+        <div className="playlist-intro">
+          <h2>{playlist.name}</h2>
+          <p>{playlist.description}</p>
+        </div>
       </div>
-      {songDetails.map((songDetail, index) => (
-        <SongResultItem
-          key={songDetail.id}
-          index={index + 1}
-          songName={songDetail.name}
-          artists={songDetail.ar}
-          album={songDetail.al}
-        />
-      ))}
+      <p>共{playlist.trackCount}首歌曲</p>
+      <div className="playlist-tracks">
+        {songDetails.map((songDetail, index) => (
+          <SongResultItem
+            key={songDetail.id}
+            index={index + 1}
+            songName={songDetail.name}
+            artists={songDetail.ar}
+            album={songDetail.al}
+          />
+        ))}
+      </div>
       <style jsx>
         {`
           .playlist-header {
-            margin-bottom: 20px;
+            margin-bottom: 50px;
+            display: flex;
+          }
+          .img-container {
+            flex: 0 0 200px;
+            width: 200px;
+            height: 200px;
+            margin-right: 25px;
+          }
+          .img-container img {
+            border-radius: 5px;
+          }
+          .playlist-intro p {
+            color: #999999;
+          }
+          .playlist-tracks {
+            margin: 10px 0 50px;
           }
         `}
       </style>
